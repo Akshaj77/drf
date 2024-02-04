2197f6ff0276f42e97080fe2292071d87db4cfa3
Akshaj77 <cse21008@iiitkalyani.ac.in>
2024-02-04 17:23:48 +0530

    create list view

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('',views.ProductCreateView.as_view()),
+    path('',views.ProductListCreateView.as_view()),
     path('<int:pk>/',views.ProductDetailAPIView.as_view())
     
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -4,9 +4,30 @@ from .models import Product
 from .serializers import ProductSerializer
 
 
-class ProductCreateView(generics.CreateAPIView):
+class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        #serializer.save(user = self.request.user) 
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+
+        serializer.save(content = content)
+        # send a Django signal
+
+
+class ProductListAPIView(generics.ListAPIView):
+    '''
+    Not gonna use
+    '''
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
